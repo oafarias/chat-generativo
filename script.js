94b7a62b75fb2e936b73369d308689650835fc6e
Alef Farias <alef.alves1800@gmail.com>
2026-02-28 16:37:33 -0300

    Add new image file 'mic.jpg' to the project

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//JAVASCRIPT - CHATZZERIA
-
 const apiKey = "6ArLGYzuwwMzltGxKft98M7gkcKw9hBMHUfbNs87XGjYKxks1aD2JQQJ99CBACYeBjFXJ3w3AAABACOGbwGp";
 
 
@@ -22,12 +20,82 @@ function callAzureOpenAI(pergunta2) {
     messages: [
       {
         role: "system",
-        content: `Você é um especialista em pizzas e só pode responder sobre esse assunto. 
-Se a pergunta não for relacionada a pizzas, responda: 
-"Desculpe, só posso responder sobre as pizzas disponíveis." 
-Informe sempre que existem três tipos de massa (Tradicional, Fina e Integral) 
-e três tamanhos (Média - 6 fatias, Grande - 8 fatias, Família - 12 fatias). 
-Seja sempre amigável e objetivo em suas recomendações.`,
+        content: `Voce é um concierge virtual do shopping Paulista Prime. E sua função é ajudar os clientes a encontrar lojas, produtos, serviços e eventos no shopping. Você tem acesso a um banco de dados atualizado com as informações do shopping, incluindo localização das lojas, horários de funcionamento e segmento das loja. Responda às perguntas dos clientes de forma clara e amigável, fornecendo informações precisas e úteis para melhorar a experiência de compra no shopping Paulista Prime.
+        Você não tem acesso a informações pessoais dos clientes, e deve respeitar a privacidade e segurança dos dados. Se um cliente fizer uma pergunta que não esteja relacionada ao shopping ou que envolva informações pessoais, responda educadamente que não pode ajudar com essa questão.
+
+        Lista de lojas do shopping Paulista Prime:
+        [
+  {
+    "id": 1,
+    "nome": "Estilo Urbano",
+    "categoria": "Moda Masculina",
+    "horario_funcionamento": "10h às 22h",
+    "box": 101
+  },
+  {
+    "id": 2,
+    "nome": "Bella Donna",
+    "categoria": "Moda Feminina",
+    "horario_funcionamento": "10h às 22h",
+    "box": 102
+  },
+  {
+    "id": 3,
+    "nome": "TechWorld",
+    "categoria": "Eletrônicos",
+    "horario_funcionamento": "10h às 22h",
+    "box": 205
+  },
+  {
+    "id": 4,
+    "nome": "Mundo Kids",
+    "categoria": "Moda Infantil",
+    "horario_funcionamento": "10h às 21h",
+    "box": 118
+  },
+  {
+    "id": 5,
+    "nome": "Sabor & Cia",
+    "categoria": "Alimentação",
+    "horario_funcionamento": "11h às 23h",
+    "box": 310
+  },
+  {
+    "id": 6,
+    "nome": "Livraria Horizonte",
+    "categoria": "Livraria",
+    "horario_funcionamento": "10h às 22h",
+    "box": 221
+  },
+  {
+    "id": 7,
+    "nome": "FitPower",
+    "categoria": "Artigos Esportivos",
+    "horario_funcionamento": "10h às 22h",
+    "box": 134
+  },
+  {
+    "id": 8,
+    "nome": "Glamour Cosméticos",
+    "categoria": "Perfumaria",
+    "horario_funcionamento": "10h às 22h",
+    "box": 156
+  },
+  {
+    "id": 9,
+    "nome": "Casa & Conforto",
+    "categoria": "Utilidades Domésticas",
+    "horario_funcionamento": "10h às 22h",
+    "box": 178
+  },
+  {
+    "id": 10,
+    "nome": "Studio Vision",
+    "categoria": "Ótica",
+    "horario_funcionamento": "10h às 22h",
+    "box": 199
+  }
+]`,
       },
       {
         // "user" é a mensagem enviada pelo usuário
@@ -72,26 +140,26 @@ Seja sempre amigável e objetivo em suas recomendações.`,
 }
 
 function addMessageToChat(className, messageContent) {
+  const htmlContent = marked.parse(messageContent);
+
   if (className === "div_card_conversa_chat") {
-    secaoConversa.innerHTML += `
-<div class="div_card_conversa" id="${className}">
-<button type="button">
-<img id="img_audio" src="../img/audio.svg" alt="Botão de áudio">
-</button>
-
-<p>${messageContent}</p>
-
-<img id="img_bot" src="../img/bot.svg" alt="Imagem do robô">
-</div>
-`;
-  } else {
-    secaoConversa.innerHTML += `
-<div class="div_card_conversa" id="${className}">
-<p>${messageContent}</p>
-</div>
-`;
-  }
-  secaoConversa.scrollTop = secaoConversa.scrollHeight;
+        secaoConversa.innerHTML += `
+            <div class="div_card_conversa" id="${className}">
+                <button type="button">
+                    <img id="img_audio" src="../img/audio.svg" alt="Botão de áudio">
+                </button>
+                <div class="markdown-body">${htmlContent}</div>
+                <img id="img_bot" src="../img/bot.svg" alt="Imagem do robô">
+            </div>
+        `;
+    } else {
+        secaoConversa.innerHTML += `
+            <div class="div_card_conversa" id="${className}">
+                <div class="markdown-body">${htmlContent}</div>
+            </div>
+        `;
+    }
+    secaoConversa.scrollTop = secaoConversa.scrollHeight;
 }
 
 document.querySelector("form").addEventListener("submit", (event) => {
